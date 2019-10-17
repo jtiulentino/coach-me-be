c72fcf8de6eceabbfb39d43831951b1bd7d5dfff
masonkarsevar <masonkarsevar@gmail.com>
2019-10-17 08:58:03 -0700

    mock login

--- routes/basicRoute.js
+++ routes/basicRoute.js
@@ -1,9 +1,15 @@
 const express = require('express');
 
-const router = express.Router()
+const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.status(200).json({message: 'hello from basic route'})
-})
+    res.status(200).json({ message: 'hello from basic route' });
+});
+
+router.post('/login', (req, res) => {
+    axios.get(
+        `https://api.airtable.com/v0/app3X8S0GqsEzH9iW/Master?api_key=${process.env.AIRTABLE_KEY}`
+    );
+});
 
 module.exports = router;
