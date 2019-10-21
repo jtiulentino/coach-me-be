c7a33c80f8a3b29d655369aac63ee00e68e5e5d9
Pat Palmerston <patpalmerston@gmail.com>
2019-10-21 13:43:39 -0700

    validateMetrics middleware is working

--- routes/basicRoute.js
+++ routes/basicRoute.js
@@ -4,7 +4,9 @@ const axios = require('axios');
 const { generateToken, authenticateToken } = require('./authenticate');
 const {
     loginMiddleware,
-    reformatPhoneNumber
+    reformatPhoneNumber,
+    validateMetrics,
+    overUnderPressureValidation
 } = require('./clientMiddleware.js');
 
 const router = express.Router();
@@ -122,24 +124,32 @@ router.get('/getMetrics', authenticateToken, (req, res) => {
         });
 });
 
-router.post('/logMetrics', authenticateToken, (req, res) => {
-    // res.status(200).json({ message: 'needs something!!!!' });
+router.post(
+    '/logMetrics',
+    authenticateToken,
+    validateMetrics,
+    overUnderPressureValidation,
+    (req, res) => {
+        // res.status(200).json({ message: 'needs something!!!!' });
 
-    axios
-        .post(
-            `https://api.airtable.com/v0/${process.env.AIRTABLE_REFERENCE}/Outcomes`,
-            req.body,
-            requestOptions
-        )
-        .then(results => {
-            res.status(201).json({
-                message: `record has been added for patient ${req.clientInfo.clientName}`
+        axios
+            .post(
+                `https://api.airtable.com/v0/${process.env.AIRTABLE_REFERENCE}/Outcomes`,
+                req.body,
+                requestOptions
+            )
+            .then(results => {
+                // console.log('logMetrics', results)
+                res.status(201).json({
+                    message: `record has been added for patient ${req.clientInfo.clientName}`
+                });
+            })
+            .catch(err => {
+                console.log('error', err);
+                res.status(500).json({ error: err });
             });
-        })
-        .catch(err => {
-            res.status(500).json({ error: err });
-        });
-});
+    }
+);
 
 module.exports = router;
 
--- routes/clientMiddleware.js
+++ routes/clientMiddleware.js
@@ -30,7 +30,68 @@ function reformatPhoneNumber(req, res, next) {
     next();
 }
 
+function validateMetrics(req, res, next) {
+    if (Object.keys(req.body.records[0].fields).length > 2) {
+        // console.log(req.body.records[0].fields.Blood_sugar);
+        // res.status(200).json({ message: 'It works!!!' });
+
+        let validMetrics = true;
+
+        for (metric in req.body.records[0].fields) {
+            // console.log('from metric for in loop', metric);
+            if (
+                metric === 'Blood_pressure_over' ||
+                metric === 'Blood_pressure_under' ||
+                metric === 'Blood_sugar' ||
+                metric === 'Weight'
+            ) {
+                if (req.body.records[0].fields[metric].toString().length > 3) {
+                    // console.log('from metric for of loop', metric);
+                    validMetrics = false;
+                }
+            }
+        }
+
+        if (validMetrics) {
+            next();
+        } else {
+            res.status(422).json({
+                error: 'One of the values is over three digits'
+            });
+        }
+    } else {
+        res.status(422).json({
+            error: 'less than three keys inside records.fields(no metrics)'
+        });
+    }
+}
+
+
+
 module.exports = {
     loginMiddleware,
-    reformatPhoneNumber
+    reformatPhoneNumber,
+    validateMetrics,
+    overUnderPressureValidation
 };
+
+// experiment data
+// {
+//     "records": [
+
+//       {
+//         "fields": {
+//           "Client_Name": [
+//             "rec8DkcsKev4Q8EvF"
+//           ],
+//           "Date_time": null,
+//   "Blood_sugar":123435643561234,
+//   "Blood_pressure_over":1421341223,
+//   "Blood_pressure_under":12321342134555,
+//   "Weight":1234
+//         }
+//       }
+
+//     ]
+
+//    }
