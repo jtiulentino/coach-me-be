c47694c0a9327ab2bd0055cb733cdc8ab1cb9ca7
masonkarsevar <masonkarsevar@gmail.com>
2019-10-22 09:20:59 -0700

    migration tables created

--- data/migrations/20191022085248_create-init-table.js
+++ data/migrations/20191022085248_create-init-table.js
@@ -1,8 +1,12 @@
-
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('patient-login', tbl => {
+        tbl.increments();
+        tbl.string('phoneNumber', 128).notNullable();
+        tbl.string('clientId', 128).notNullable();
+        tbl.string('loginTime', 500);
+    });
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('patient-login');
 };
