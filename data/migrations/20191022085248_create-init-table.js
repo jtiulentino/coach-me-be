c380aa6f931ce6178b6bd643e10ad2230f6a08ae
masonkarsevar <masonkarsevar@gmail.com>
2019-10-23 13:12:31 -0700

    first commit on branch

--- data/migrations/20191022085248_create-init-table.js
+++ data/migrations/20191022085248_create-init-table.js
@@ -1,12 +1,51 @@
 exports.up = function(knex) {
-    return knex.schema.createTable('patient-login', tbl => {
-        tbl.increments();
-        tbl.string('phoneNumber', 128).notNullable();
-        tbl.string('clientId', 128).notNullable();
-        tbl.string('loginTime', 500);
-    });
+    return knex.schema
+        .createTable('patient-login', tbl => {
+            tbl.increments();
+            tbl.string('phoneNumber', 128).notNullable();
+            tbl.string('clientId', 128).notNullable();
+            tbl.string('loginTime', 500);
+        })
+        .createTable('users', tbl => {
+            tbl.primary().uuid('userId');
+            tbl.string('role', 255);
+            tbl.string('userPhone', 255).notNullable();
+        })
+        .createTable('coaches', tbl => {
+            tbl.primary().uuid('coachId');
+            tbl.string('coachName', 255).notNullable();
+            tbl.string('userId')
+                .notNullable()
+                .unsigned()
+                .references('userId')
+                .inTable('users')
+                .onDelete('CASCADE')
+                .onUpdate('CASCADE');
+        })
+        .createTable('patients', tbl => {
+            tbl.primary().uuid('patientId');
+            tbl.string('userId')
+                .notNullable()
+                .unsigned()
+                .references('userId')
+                .inTable('users')
+                .onDelete('CASCADE')
+                .onUpdate('CASCADE');
+            tbl.string('coachId', 255)
+                .notNullable()
+                .unsigned()
+                .references('coachId')
+                .inTable('coaches')
+                .onDelete('CASCADE')
+                .onUpdate('CASCADE');
+            tbl.string('patientName', 255);
+        });
 };
 
 exports.down = function(knex) {
-    return knex.schema.dropTableIfExists('patient-login');
+    return knex.schema
+        .dropTableIfExists('patients')
+        .dropTableIfExists('coaches')
+        .dropTableIfExists('users')
+        .dropTableIfExists('patient-login');
 };
