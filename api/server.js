d93162fa549497d7eeb8817e434cffd594e928a3
masonkarsevar <masonkarsevar@gmail.com>
2019-11-06 14:37:04 -0800

    twilio routes uncommented

--- api/server.js
+++ api/server.js
@@ -4,7 +4,7 @@ const configureMiddleware = require('./middleware');
 const basicRoute = require('../routes/basicRoute.js');
 const coachRoute = require('../routes/coachRoute/coachRoute.js');
 const forgotRoute = require('../routes/forgotPassword.js');
-// const twilioRoute = require('../routes/twilioRoute/twilioroute.js');
+const twilioRoute = require('../routes/twilioRoute/twilioroute.js');
 
 const server = express();
 
@@ -17,7 +17,7 @@ server.use('/coachRoute', coachRoute);
 server.use('/forgotRoute', forgotRoute);
 
 // having trouble testing this endpoint.
-// server.use('/twilioRoute', twilioRoute);
+server.use('/twilioRoute', twilioRoute);
 
 server.get('/', (req, res) => {
     res.status(200).json({ message: 'hello world from base server' });
