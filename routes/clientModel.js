540f9f2272d2f37c67a805538b0722e32099eeaf
masonkarsevar <masonkarsevar@gmail.com>
2019-11-12 12:35:49 -0800

    all returns in insert functions deleted

--- routes/clientModel.js
+++ routes/clientModel.js
@@ -1,27 +1,27 @@
-const db = require('../data/dbConfig.js');
+const db = require("../data/dbConfig.js");
 
 module.exports = {
-    findPatientByPhone,
-    updateLoginTime,
-    insertNewClient
+  findPatientByPhone,
+  updateLoginTime,
+  insertNewClient
 };
 
 // filter by clientPhone using the phoneNumber key in the patient-login db
 function findPatientByPhone(filter) {
-    return db('patient-login').where(filter);
+  return db("patient-login").where(filter);
 }
 
 // needs to update the LoginTime associated with any phoneNumber used to log in
 function updateLoginTime(filter, changes) {
-    return db('patient-login')
-        .where(filter)
-        .update(changes, 'id');
+  return db("patient-login")
+    .where(filter)
+    .update(changes);
 }
 
 function insertNewClient(filter) {
-    return db('patient-login').insert(filter, 'id');
-    // .then(res => {
-    //     const [client] = res;
-    //     return findPatientByPhone({ id: client });
-    // });
+  return db("patient-login").insert(filter);
+  // .then(res => {
+  //     const [client] = res;
+  //     return findPatientByPhone({ id: client });
+  // });
 }
