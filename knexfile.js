2b3a0744c68550582a0a772f4dfec2ef1b13bdfb
jjgorton <jjgorton@gmail.com>
2019-11-11 19:22:55 -0500

    added production environment to knexfile to use PG

--- knexfile.js
+++ knexfile.js
@@ -36,5 +36,21 @@ module.exports = {
                 conn.run('PRAGMA foreign_keys = ON', done);
             }
         }
+    },
+    production: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL,
+        useNullAsDefault: true,
+        migrations: {
+            directory: './data/migrations'
+        },
+        seeds: {
+            directory: './data/seeds'
+        },
+        pool: {
+            afterCreate: (conn, done) => {
+                conn.run('PRAGMA foreign_keys = ON', done);
+            }
+        }
     }
 };
